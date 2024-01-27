c4352598b50e51dff81387a7b72c749327c41660
Brian Zhang <brian.w.zhang@gmail.com>
2024-01-27 10:58:06 -0500

    refactored loginscreen

--- imy/screens/LoginScreen.js
+++ imy/screens/LoginScreen.js
@@ -1,11 +1,22 @@
-import { TouchableOpacity, StyleSheet, Text, View, ImageBackground, TextInput, Button } from "react-native"
-import { useState } from 'react'
-import flappyBgImage from "../assets/flappy_bg.jpg"
-import { useFonts, PressStart2P_400Regular } from '@expo-google-fonts/press-start-2p'
-import { Prompt_400Regular } from '@expo-google-fonts/prompt'
-import { Poppins_400Regular } from '@expo-google-fonts/poppins'
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  Button,
+} from 'react-native';
+import { useState } from 'react';
+import flappyBgImage from '../assets/flappy_bg.jpg';
+import {
+  useFonts,
+  PressStart2P_400Regular,
+} from '@expo-google-fonts/press-start-2p';
+import { Prompt_400Regular } from '@expo-google-fonts/prompt';
+import { Poppins_400Regular } from '@expo-google-fonts/poppins';
 
-const LoginScreen = ({ navigation }) => {
+const LoginScreen = () => {
   let [fontsLoaded] = useFonts({
     PressStart2P_400Regular,
     Prompt_400Regular,
@@ -16,21 +27,20 @@ const LoginScreen = ({ navigation }) => {
 
   const handleLogin = () => {
     // whatever we need to do on the backend funky bs
-  }
+  };
 
   const handleForgotPassword = () => {
     // a totally real forgot password button
-  }
-  
+  };
 
   return (
     <View style={styles.container}>
       <ImageBackground style={styles.backgroundImage} source={flappyBgImage}>
         <View style={styles.welcomeContainer}>
           <Text style={styles.welcome}>Welcome</Text>
-        </View> 
+        </View>
         <View style={styles.inputContainer}>
-                  <TextInput
+          <TextInput
             style={styles.input}
             onChangeText={setUsername}
             value={username}
@@ -45,49 +55,50 @@ const LoginScreen = ({ navigation }) => {
             placeholderTextColor="white" // Add this line
             secureTextEntry
           />
-          <Text style={styles.forgotPassword} onPress={handleForgotPassword}>Forgot Password?</Text>
-          </View>
-          <View style={styles.buttonContainer}>
+          <Text style={styles.forgotPassword} onPress={handleForgotPassword}>
+            Forgot Password?
+          </Text>
+        </View>
+        <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.enterButton} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Enter</Text>
-        </TouchableOpacity>
+            <Text style={styles.buttonText}>Enter</Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   welcome: {
-    fontFamily: "PressStart2P_400Regular",
-    color: "white",
-    fontSize: "40%",
-  }, 
+    fontFamily: 'PressStart2P_400Regular',
+    color: 'white',
+    fontSize: '40%',
+  },
   container: {
     flex: 1,
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
   },
   backgroundImage: {
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
   },
   welcomeContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "40%", 
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '40%',
   },
   inputContainer: {
     flex: 2,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: "-5%",
-    
+    marginTop: '-5%',
   },
   input: {
     height: 40,
-    width: "80%",
+    width: '80%',
     borderColor: 'white',
     borderWidth: 1,
     marginTop: 10,
@@ -95,29 +106,29 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     color: 'white',
   },
-  placeholder:{
-    fontFamily: "Prompt_400Regular",
+  placeholder: {
+    fontFamily: 'Prompt_400Regular',
   },
   forgotPassword: {
     color: 'white',
     marginTop: 20,
     textDecorationLine: 'underline',
-    fontFamily: "Poppins_400Regular",
+    fontFamily: 'Poppins_400Regular',
   },
   buttonContainer: {
     flex: 0.5,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: "80%",
+    marginBottom: '80%',
   },
   enterButton: {
-    backgroundColor: "#0CA41C",
+    backgroundColor: '#0CA41C',
     padding: 10,
     borderRadius: 15,
     alignItems: 'center',
-    width: "70%",
-    textAlign: "center",
-    shadowColor: "#000",
+    width: '70%',
+    textAlign: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 7,
@@ -128,9 +139,9 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: 'white',
-    fontFamily: "PressStart2P_400Regular",
+    fontFamily: 'PressStart2P_400Regular',
     fontSize: 20,
   },
-})
+});
 
-export default LoginScreen
+export default LoginScreen;
