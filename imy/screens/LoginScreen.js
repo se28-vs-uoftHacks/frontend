98c4193719d144eddecd4cb9621c77c2daca67fc
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2024-01-27 02:33:16 -0500

    started login page

--- imy/screens/LoginScreen.js
+++ imy/screens/LoginScreen.js
@@ -1,30 +1,33 @@
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import flappyBgImage from '../assets/flappy_bg.jpg'
+import { StyleSheet, Text, View, ImageBackground } from "react-native"
+import flappyBgImage from "../assets/flappy_bg.jpg"
 
 const LoginScreen = ({ navigation }) => {
-
-    return (
-        <ImageBackground style={styles.backgroundImage} source={flappyBgImage}>
-            <View style={styles.container}>
-                <Text>Login Screen</Text>
-            </View>
-        </ImageBackground>
-    )
-
+  return (
+    <View style={styles.container}>
+      <ImageBackground style={styles.backgroundImage} source={flappyBgImage}>
+        <View style={styles.innerContainer}>
+          <Text>Login Screen</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    backgroundImage: {
-        flex: 1,
-        resizeMode: 'cover',
-    },
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
 
 export default LoginScreen
-
